53a3789c91bb92a125f3f5bf85dffb516d08c954
osKlonowski <38720852+osKlonowski@users.noreply.github.com>
2019-03-12 11:32:14 +0100

    adding an avatar icon and updating keyboard functions

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from pygame.locals import *
 import sys, pygame
 pygame.init()
 
@@ -7,9 +8,91 @@ black = 0, 0, 0
 
 screen = pygame.display.set_mode(size)
 
-while 1:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+class Player:
+    x = 10
+    y = 10
+    speed = 1
+ 
+    def moveRight(self):
+        self.x = self.x + self.speed
+ 
+    def moveLeft(self):
+        self.x = self.x - self.speed
+ 
+    def moveUp(self):
+        self.y = self.y - self.speed
+ 
+    def moveDown(self):
+        self.y = self.y + self.speed
 
-    screen.fill(black)
-    pygame.display.flip()
+class App:
+    windowWidth = 800
+    windowHeight = 600
+    player = 0
+ 
+    def __init__(self):
+        self._running = True
+        self._display_surf = None
+        self._image_surf = None
+        self.player = Player()
+ 
+    def on_init(self):
+        pygame.init()
+        self._display_surf = pygame.display.set_mode((self.windowWidth,self.windowHeight), pygame.HWSURFACE)
+ 
+        pygame.display.set_caption('MAZED')
+        self._running = True
+        self._image_surf = pygame.image.load("avatar.png").convert()
+ 
+    def on_event(self, event):
+        if event.type == QUIT:
+            self._running = False
+ 
+    def on_loop(self):
+        pass
+ 
+    def on_render(self):
+        self._display_surf.fill((255,255,255))
+        self._display_surf.blit(self._image_surf,(self.player.x,self.player.y))
+        pygame.display.flip()
+ 
+    def on_cleanup(self):
+        pygame.quit()
+ 
+    def on_execute(self):
+        if self.on_init() == False:
+            self._running = False
+ 
+        while( self._running ):
+            pygame.event.pump()
+            keys = pygame.key.get_pressed()
+ 
+            if (keys[K_RIGHT]):
+                self.player.moveRight()
+ 
+            if (keys[K_LEFT]):
+                self.player.moveLeft()
+ 
+            if (keys[K_UP]):
+                self.player.moveUp()
+ 
+            if (keys[K_DOWN]):
+                self.player.moveDown()
+ 
+            if (keys[K_ESCAPE]):
+                self._running = False
+ 
+            self.on_loop()
+            self.on_render()
+        self.on_cleanup()
+ 
+if __name__ == "__main__" :
+    theApp = App()
+    theApp.on_execute()
+
+# while 1:
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT: sys.exit()
+
+#     screen.fill(black)
+#     pygame.display.flip()
