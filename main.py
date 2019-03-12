d26af6784ce7b5b073bf2b420089e1e4d093848c
osKlonowski <38720852+osKlonowski@users.noreply.github.com>
2019-03-12 11:13:21 +0100

    creating a window

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import sys, pygame
 pygame.init()
 
-size = width, height = 320, 240
+size = width, height = 960, 960
 speed = [2, 2]
 black = 0, 0, 0
 
 screen = pygame.display.set_mode(size)
+
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+
+    screen.fill(black)
+    pygame.display.flip()
